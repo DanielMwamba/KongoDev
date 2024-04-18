7fc297a2fd6c1d1ea2421257b36401f4681df497
Mwamba <daniel.mwamba@iMac-de-kadea-17.local>
2024-04-18 14:35:36 +0100

    feat: Add categories

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,6 @@
 import Hero from "../components/hero";
+import CategoryCard from "../components/categoryCard";
+import categories from  "../services/api/categories.json";
 import { Button } from "@material-tailwind/react";
 
 export default function Home() {
@@ -14,6 +16,24 @@ export default function Home() {
         </span>
       </div>
 
+      <section className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+         <div className="grid grid-cols-2 grid-rows-2 gap-5 sm:grid-cols-3 sm:grid-rows-2">
+          {categories.slice(0,5).map(category=>{
+            return <CategoryCard
+            key= {category.id}
+            name={category.name}
+            image={category.imageURL}
+            link={`/categories/${category.name}`}
+          />
+
+          })}
+          <CategoryCard
+                name="Tout explorer"
+                image="https://plus.unsplash.com/premium_photo-1670426501357-23bbaaab1e3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80"
+                link="/categories/"
+              />
+         </div>
+      </section>
 
       <div className="flex justify-center items-center pt-20 pb-10">
         <span
