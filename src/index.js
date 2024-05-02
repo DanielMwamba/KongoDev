caa32c3dc41470677b77ae4e75116dca4179c319
Dan Mwamba <danmuamba81@gmail.com>
2024-05-02 15:43:15 +0200

    feat: config cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const userRouter = require("./routes/user.routes")
 const postRouter = require("./routes/post.routes")
@@ -13,8 +14,12 @@ const postRouter = require("./routes/post.routes")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const corsOption = {
+    origin: "*"
+};
 
 app.use(express.json());
+app.use(cors(corsOption));
 app.use((req, res, next) => {
     res.header(
         "Access-Control-Allow-Headers",
