798cb0d116b705a9697800f48e2e45b629af78a1
Dan Mwamba <danmuamba81@gmail.com>
2024-05-05 02:11:46 +0200

    feat: add functions that make API calls to retrieve user data and the post

--- src/services/api/api.jsx
+++ src/services/api/api.jsx
@@ -15,6 +15,15 @@ export async function getUser() {
   }
 }
 
+export async function refreshToken(refreshToken) {
+  try {
+    const response = await axios.post(`https://kongodevapi.onrender.com/api/user/refreshToken`,{refreshToken});
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 
 
 
@@ -27,5 +36,51 @@ export async function getAllPosts() {
       console.log(
         `Error while Fetching Particular User Data using GET API Method : ${err}`
       );
+    }
+  }
+
+  export async function addPost(data) {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.post(`https://kongodevapi.onrender.com/api/post`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return Promise.resolve(response.data);
+    } catch (error) {
+      const errorMsg = error.response ? error.response.data.msg : error.message;
+      return Promise.reject({ msg: errorMsg });
+    }
+  }
+
+
+  export async function updatePost(post_id, data) {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.put(`https://kongodevapi.onrender.com/api/post/${post_id}`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return Promise.resolve(response.data);
+    } catch (error) {
+      const errorMsg = error.response ? error.response.data.msg : error.message;
+      return Promise.reject({ msg: errorMsg });
+    }
+  }
+
+  export async function getUserPosts() {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.get(`https://kongodevapi.onrender.com/api/post/user-all`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return Promise.resolve(response.data);
+    } catch (error) {
+      const errorMsg = error.response ? error.response.data.msg : error.message;
+      return Promise.reject({ msg: errorMsg });
     }
   }
