87f0f5e12613d938461b9e3132ab02042bf2e00f
Dan Mwamba <danmuamba81@gmail.com>
2024-05-06 08:08:04 +0200

    refactor: correction of titles and paragraphs

--- src/author_panel/components/emptyPostState.panel.jsx
+++ src/author_panel/components/emptyPostState.panel.jsx
@@ -20,8 +20,8 @@ export default function EmptyPostState() {
           d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
         />
       </svg>
-      <h3 className="mt-2 text-sm font-medium text-gray-900">No Posts</h3>
-      <p className="mt-1 text-sm text-gray-500">Commencer par écrire un nouveau article.</p>
+      <h3 className="mt-2 text-sm font-medium text-gray-900">Aucun de poste</h3>
+      <p className="mt-1 text-sm text-gray-500">Commencer par écrire ce qui vous passionne.</p>
       <div className="mt-6">
         <Link
           to="/authorpanel/blogs/new"
@@ -29,7 +29,7 @@ export default function EmptyPostState() {
           className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
           <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-          Nouveau article
+          Ecrire un nouvel article
         </Link>
       </div>
     </div>
