f5d13d5eea0505a20001191cc7c46532926e8fc3
Daniel Mwamba <danmuamba81@gmail.com>
2025-01-02 16:49:09 +0100

    fix: add @vercel/speed-insights package and integrate it in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 // import "./pages/css/blogStyle.css";
-import { ThemeProvider } from '@material-tailwind/react';
-import {Provider} from 'react-redux';
-import {store, persistedStore} from './redux/store.jsx';
-import {PersistGate} from "redux-persist/integration/react";
+import { ThemeProvider } from "@material-tailwind/react";
+import { Provider } from "react-redux";
+import { store, persistedStore } from "./redux/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <ThemeProvider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider>
     <Provider store={store}>
-      <PersistGate persistor= {persistedStore}>
+      <PersistGate persistor={persistedStore}>
+        <SpeedInsights framework="react" />
         <React.StrictMode>
-          <main className=''>
-          <App/>
+          <main className="">
+            <App />
           </main>
-          
         </React.StrictMode>
-      </PersistGate> 
-    </Provider> 
- </ThemeProvider>
- 
-)
+      </PersistGate>
+    </Provider>
+  </ThemeProvider>
+);
